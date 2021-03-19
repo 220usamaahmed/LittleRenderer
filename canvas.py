a35352f630fe263be1503ea42fcdbd2891c4f023
Usama Ahmed <usiddiquie.bscs18seecs@seecs.edu.pk>
2021-03-19 17:14:40 +0500

    Added darw line in canvas

--- canvas.py
+++ canvas.py
@@ -1,19 +1,43 @@
-from typing import Tuple
-
 from PIL import Image, ImageDraw
 
 class Canvas:
 
-    def __init__(self, dimensions: Tuple[int, int] = (512, 512)):
+    def __init__(self, dimensions=(512, 512)):
         self.dimensions = dimensions
         self.image = Image.new("RGB", dimensions)
         self.imageDraw = ImageDraw.Draw(self.image)
 
 
-    def point(self, v: Tuple[int, int], color: Tuple[int, int, int]):
+    def point(self, v, color):
         x = self.dimensions[0] / 2 + v[0]
         y = self.dimensions[1] / 2 - v[1]
         self.imageDraw.point((x, y), fill=color)
+
+
+    def line(self, v0, v1, color):
+        steep = False
+
+        if abs(v0[0] - v1[0]) < abs(v0[1] - v1[1]):
+            steep = True
+            v0[0], v0[1] = v0[1], v0[0]
+            v1[0], v1[1] = v1[1], v1[0]
+
+        if v0[0] > v1[0]:
+            v0, v1 = v1, v0
+
+        dx = v1[0] - v0[0]
+        dy = v1[1] - v0[1]
+        error = 0
+        d_error = abs(dy) * 2
+        y = v0[1]
+
+        for x in range(v0[0], v1[0]):
+            if steep: self.point([y, x], color)
+            else: self.point([x, y], color)
+            error += d_error
+            if error > dx:
+                y += 1 if v1[1] > v0[1] else -1
+                error -= dx * 2
 
 
     def show(self):
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from canvas import Canvas
 
 
 def main():
-    canvas = Canvas()
-    canvas.point((0, 0), (255, 255, 255))
+    canvas = Canvas()    
+    canvas.line([0, 100], [100, -50], (255, 255, 255))
+    canvas.line([100, -50], [-100, -50], (255, 255, 255))
+    canvas.line([-100, -50], [0, 100], (255, 255, 255))
     canvas.show()
 
 
